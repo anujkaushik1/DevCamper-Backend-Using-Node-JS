52a0f449d954b6e2943fc36cae74255d27726828
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-01-13 13:34:14 +0530

    Implemented Router for Routes

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route Files =>
+
+const bootcamps = require("./routes/bootcamps")
+
 //LOADING ENV VARIABLES =>
 
 dotenv.config({ path: "./config/config.env" });
@@ -8,6 +12,9 @@ dotenv.config({ path: "./config/config.env" });
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Mounting Routes =>
+
+app.use("/api/v1/bootcamps", bootcamps);
 
 app.listen(PORT, function(){
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
