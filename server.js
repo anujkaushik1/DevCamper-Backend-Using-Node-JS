7843cb7d6f03a9c4324ec319bda352757adbff87
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-01-29 20:59:46 +0530

    Established Connection with MongoDB

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const logger = require("./middleware/logger");
+const connectDB = require("./config/db");
+
+// Loading Env Variables =>
+
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to Database =>
+
+connectDB();
+
 
 // Route Files =>
 
 const bootcamps = require("./routes/bootcamps")
-
-//LOADING ENV VARIABLES =>
-
-dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -21,6 +27,16 @@ app.use(logger);
 
 app.use(API, bootcamps);
 
-app.listen(PORT, function(){
+const server = app.listen(PORT, function(){
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 })
+
+// Handle Unhandled Promise Rejection =>
+
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error : ${err.message}`);
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
