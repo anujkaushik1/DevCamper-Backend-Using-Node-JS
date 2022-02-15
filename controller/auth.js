35487dd7912462958469405b1f20c129f9dd598e
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-15 18:33:40 +0530

    Implemented User Registration and Encrypted Passwords using Bcrypt

--- controller/auth.js
+++ controller/auth.js
@@ -7,7 +7,18 @@ const ErrorResponse = require("../utils/errorResponse");
 // @access    Public
 
 exports.register = asyncHandler(async function(req, res, next){
+    const { name, email, password, role } = req.body;
+
+    // Create user
+    const user = await User.create({
+        name,
+        email,
+        password,
+        role
+    });
+
     res.status(200).json({
-        success : true
+        success : true,
     })
+
 });
