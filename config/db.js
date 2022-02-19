24ed28ca13be767f5517201f27a1f1c1f70ffb07
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-19 10:32:00 +0530

    Structured Database Code

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
-const connectDB = async function(){
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+const connectDB = async function () {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected : ${conn.connection.host}`);
-}
+  console.log(`MongoDB Connected : ${conn.connection.host}`);
+};
 
 module.exports = connectDB;
