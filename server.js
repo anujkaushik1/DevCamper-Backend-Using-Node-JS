eb64ba0d23216d757343cf2b4b6d331084613468
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-02-22 19:16:51 +0530

    Implemented Rate Limiting & HPP

--- server.js
+++ server.js
@@ -8,18 +8,17 @@ const cookieParser = require("cookie-parser");
 const mongoSanitize = require("express-mongo-sanitize");
 const helmet = require("helmet");
 const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 const errorHandler = require("./middleware/error");
 
 // Loading Env Variables =>
-
 dotenv.config({ path: "./config/config.env" });
 
 // Connect to Database =>
-
 connectDB();
 
 // Route Files =>
-
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
 const auth = require("./routes/auth");
@@ -39,27 +38,32 @@ const PORT = process.env.PORT || 5000;
 app.use(logger);
 
 // File Upload =>
-
 app.use(fileupload());
 
-// Sanitize data 
-
+// Sanitize data
 app.use(mongoSanitize());
 
 // Set security header
-
 app.use(helmet());
 
 // Prevent XSS attacks
-
 app.use(xss());
 
-// Set static folder =>
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 minutes
+  max: 100,
+});
 
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Set static folder =>
 app.use(express.static(path.join(__dirname, "public")));
 
 // Mounting Routes =>
-
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 app.use("/api/v1/auth", auth);
@@ -75,7 +79,6 @@ const server = app.listen(PORT, function () {
 });
 
 // Handle Unhandled Promise Rejection =>
-
 process.on("unhandledRejection", (err, promise) => {
   console.log(`Error : ${err.message}`);
   server.close(() => {
